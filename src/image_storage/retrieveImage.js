dd7d880df40573bca2fc59875de638e2c90e1200
Neil Dudani <neil.dudani@gmail.com>
2021-07-13 22:17:49 -0400

    Stuck on how to send images back to client

--- src/image_storage/retrieveImage.js
+++ src/image_storage/retrieveImage.js
@@ -3,21 +3,25 @@ const fs = require('fs');
 const { Grid, conn } = require('../config/mongo');
 
 function retrieveImage(mongoId) {
-  const gfs = Grid(conn.db);
+  return new Promise((resolve) => {
+    const gfs = Grid(conn.db);
 
-  // Write content from DB to file system with the given name
-  const fs_write_stream = fs.createWriteStream(path.join(__dirname, '../../image_examples/retrieval/hr_logo_retrieved.jpeg'));
+    // Write content from DB to file system with the given name
+    const pathname = path.join(__dirname, `../../image_examples/retrieval/${mongoId}.jpg`);
+    const fs_write_stream = fs.createWriteStream(pathname);
 
-  // Create read-stream from mongodb
-  // Find the correct file by 'filename', but could also find by ID or other properties
-  const readstream = gfs.createReadStream({
-    _id: mongoId,
+    // Create read-stream from mongodb
+    // Find the correct file by 'filename', but could also find by ID or other properties
+    const readstream = gfs.createReadStream({
+      _id: mongoId,
+    });
+
+    readstream.pipe(fs_write_stream);
+    fs_write_stream.on('close', () => {
+      console.log('File has been written fully!');
+      resolve(pathname);
+    });
   });
-
-  readstream.pipe(fs_write_stream);
-  fs_write_stream.on('close', () => {
-    console.log('File has been written fully!');
-  });
-};
+}
 
 module.exports.retrieveImage = retrieveImage;
