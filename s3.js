32a95763d6e4b1c83ac820cae010cd8442b4b1d1
Neil Dudani <neil.dudani@gmail.com>
2021-07-14 11:08:42 -0400

    implemented s3 bucket

--- s3.js
+++ s3.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const fs = require('fs');
 const S3 = require('aws-sdk/clients/s3');
 
 const bucketName = process.env.AWS_BUCKET_NAME;
@@ -6,5 +8,21 @@ const accessKeyId = process.env.AWS_ACCESS_KEY;
 const secretAccessKey = process.env.AWS_SECRET_KEY;
 
 const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+});
 
-})
+function uploadFile(imagePath, storageName) {
+  const fileStream = fs.createReadStream(imagePath);
+
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: storageName,
+  };
+
+  return s3.upload(uploadParams).promise();
+}
+
+module.exports.uploadFile = uploadFile;
--- src/Controllers/shops.js
+++ src/Controllers/shops.js
@@ -1,6 +1,7 @@
-const path = require('path');
-const fs = require('fs');
+const multer = require('multer');
 const db = require('../config/db');
+const { uploadFile } = require('../../s3');
+const upload = multer({ dest: 'image_storage/' });
 
 const getShops = (req, res) => {
   const count = req.body.count || 9;
@@ -119,26 +120,26 @@ const getRecentShops = async (req, res) => {
   ;
   `;
 
-  let data = await db.query(sqlQuery, [count, page * count]);
+  const data = await db.query(sqlQuery, [count, page * count]);
   for (let i = 0; i < data.rows.length; i += 1) {
     data.rows[i].price = 3;
   }
   res.status(200).json(data.rows);
 };
 
-const addShop = (req, res) => {
+const addShop = async (req, res) => {
   const {
     name, address, city, state, zip, phone_number, website, animal_friendly,
   } = req.body;
-  const sqlQuery = `INSERT INTO shops (name, address, city, state, zip, date, phone_number, website, animal_friendly) VALUES('${name}', '${address}', '${city}', '${state}', ${zip}, current_timestamp, '${phone_number}', '${website}', '${animal_friendly}') RETURNING shop_id;`;
+  const sqlQuery1 = `INSERT INTO shops (name, address, city, state, zip, date, phone_number, website, animal_friendly) VALUES('${name}', '${address}', '${city}', '${state}', ${zip}, current_timestamp, '${phone_number}', '${website}', '${animal_friendly}') RETURNING shop_id;`;
 
-  db.query(sqlQuery, [], (err, success) => {
-    if (err) {
-      res.status(500).json(err);
-    } else {
-      res.status(201).send(success);
-    }
-  });
+  const store1 = await db.query(sqlQuery1, []);
+  const shopId = store1.rows[0].shop_id;
+  const imagePath = 'image_storage/hr_logo.jpeg';
+  const saveToS3 = await uploadFile(imagePath, name);
+  const sqlQuery2 = `INSERT INTO shops_photos (shop_id, url) VALUES (${shopId}, '${saveToS3.Location}');`;
+  await db.query(sqlQuery2);
+  res.send(store1);
 };
 
 module.exports = {
